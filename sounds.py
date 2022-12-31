b2a1c770a3bc4e83b5f416a0e8e08bb5bda54a1a
Anderson <daosantiago@gmail.com>
2022-12-31 12:36:32 +0000

    add sound

--- sounds.py
+++ sounds.py
@@ -1,15 +1,21 @@
+import pygame
 
-import time
+FILES = {
+    'menu': './assets/sound/Planetrise_LEO.wav',
+    'play': './assets/sound/background.wav',
+    'shoot': './assets/sound/tiro.wav',
+    'explode': './assets/sound/explosion.wav'
+}
 
-import pygame as pg
 
-pg.init()
-pg.mixer.init()  # Inicializa o mixer do Pygame
+class Sound():
+    def __init__(self, song) -> None:
+        self.sound = pygame.mixer.Sound(FILES[song])
+        self.sound.set_volume(0.1)
+        self.loop = -1 if song == 'play' else 0
 
-pg.mixer.music.load('./assets/BlueSpacev0_8.mp3')
-pg.mixer.music.play(-1)
-time.sleep(2)
-pg.mixer.music.stop()
+    def play(self):
+        self.sound.play(self.loop)
 
-# Reproduz o áudio
-soundtrack.play()
+    def stop(self):
+        self.sound.stop()
